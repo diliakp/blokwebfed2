d1d37e125c46ac302f28719ebe387e5fa2de27b4
diliakp <143949416+diliakp@users.noreply.github.com>
2023-09-25 10:50:22 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,18 @@
 // JavaScript Document
 console.log("hi");
+
+var scrollContainer = document.querySelector('.home article:nth-of-type(1) section:nth-of-type(2)');
+
+var buttonLeft = document.querySelector('.home article:nth-of-type(1) button:nth-of-type(1)');
+
+var buttonRight = document.querySelector('.home article:nth-of-type(1) button:nth-of-type(2)');
+
+
+
+buttonLeft.addEventListener('click', function(){
+    scrollContainer.scrollLeft -= 350;
+})
+
+buttonRight.addEventListener('click', function(){
+    scrollContainer.scrollLeft += 350;
+})
