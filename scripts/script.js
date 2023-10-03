4f0c5073b10990909467e1f49e1bbf5bbce7e9a6
diliakp <143949416+diliakp@users.noreply.github.com>
2023-10-03 13:55:28 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -8,21 +8,26 @@ var buttonLeft = document.querySelector('.home article:nth-of-type(1) button:nth
 var buttonRight = document.querySelector('.home article:nth-of-type(1) button:nth-of-type(2)');
 
 
-buttonLeft.addEventListener('click', function(){
-    scrollContainer.scrollLeft -= 350;
-});
+if(buttonLeft == 0){
+    buttonLeft.addEventListener('click', function(){
+        scrollContainer.scrollLeft -= 350;
+    });
+    
+    buttonRight.addEventListener('click', function(){
+        scrollContainer.scrollLeft += 350;
+    });    
+}else{
 
-buttonRight.addEventListener('click', function(){
-    scrollContainer.scrollLeft += 350;
-});
+}
+
 
 
 
 
 // hamburgermenu openen en sluiten
-var menuButton = document.querySelector('header button:nth-of-type(1)');
-var kruisje = document.querySelector('header button:nth-of-type(2)');
-var heleNav = document.querySelector('header nav');
+var menuButton = document.querySelector('header div button:nth-of-type(1)');
+var kruisje = document.querySelector('header div button:nth-of-type(2)');
+var heleNav = document.querySelector('header div nav');
 
 heleNav.classList.add('hidden');
 kruisje.classList.add('hidden');
@@ -43,20 +48,60 @@ menuButton.addEventListener("click", openMenu);
 kruisje.addEventListener("click", closeMenu);
 
 
-
 // section tevoorschijn laten komen na het drukken op een knop
 var bestelContainer = document.querySelector('.home section:nth-of-type(1) article:nth-of-type(3)');
 var buttonAfhalen = document.querySelector('.home section:nth-of-type(1) article:nth-of-type(2) section button:nth-child(2)');
 var buttonTerug = document.querySelector('.home section:nth-of-type(1) article:nth-of-type(3) section img')
 
-bestelContainer.classList.toggle('hidden');
 
-buttonAfhalen.addEventListener('click', function(){
+
+if(buttonAfhalen == 0){
     bestelContainer.classList.toggle('hidden');
-});
 
-buttonTerug.addEventListener('click', function(){
-    bestelContainer.classList.toggle('show');
-});
+    buttonAfhalen.addEventListener('click', function(){
+        bestelContainer.classList.toggle('hidden');
+    });
+    
+    buttonTerug.addEventListener('click', function(){
+        bestelContainer.classList.toggle('show');
+    });
+    
+}else{
+
+}
 
 
+// dark mode
+function toggleDarkMode() {
+    var body = document.body;
+    body.classList.toggle('dark-mode');
+ }
+
+ var darkModeToggle = document.querySelector('header div img:nth-of-type(1)');
+ darkModeToggle.addEventListener('click', toggleDarkMode);
+
+   
+
+
+
+// var homepage = document.querySelector('.home')
+// var pagina2 = document.querySelector('.page2')
+// if(homepage !== undefined){
+//     function toggleDarkMode() {
+//         var body = document.body;
+//         body.classList.toggle('dark-mode');
+//      }
+
+//     var darkModeToggle = document.querySelector('.home section:nth-of-type(1) article:nth-of-type(1) img:nth-of-type(1)');
+//     darkModeToggle.addEventListener('click', toggleDarkMode);
+// }else if(pagina2 !== undefined){
+//     function toggleDarkMode2() {
+//         var body = document.body;
+//         body.classList.toggle('dark-mode');
+//      }
+
+//     var darkModeToggle2 = document.querySelector('.page2 section:nth-of-type(1) img:nth-of-type(1)');
+//     darkModeToggle2.addEventListener('click', toggleDarkMode2);
+// }else{
+
+// }
